04693d2cc0c9d673f3444fa036ef6933a0d2bc9f
kevinnivek96 <kevinnivek96@gmail.com>
2018-03-04 23:21:42 +0100

    Practica 2

---
+++ cmds.js
@@ -0,0 +1,230 @@
+const model = require('./model');
+const {log,errorlog,colorize,biglog} = require('./out');
+
+
+
+exports.helpCmd = rl => {
+
+	log('Commandos : ')
+	log('	h|help - Muestra esta ayuda.');
+	log('	list - Listar los quizzes existentes.');
+	log('	show <id> - Muestra la pregunta y la respuesta del quiz indicado.');
+	log('	add - Añadir un nuevo quiz interactivamente.');
+	log('	delete <id> - Borrar el quiz indicado.');
+	log('	edit <id> - Editar el quiz indicado.');
+	log('	test <id> - Probar el quiz indicado.');
+	log('	p|play - Jugar a preguntar aleatoriamente todos los quizzes.');
+	log('	credits - Créditos.');
+	log('	q|quit - Salir del programa.');
+	rl.prompt();
+};
+
+exports.listCmd = rl => {
+	model.getAll().forEach((quiz,id) => {
+
+		log(` [${colorize(id,'magenta')}]: ${quiz.question}`);
+
+	});
+	rl.prompt();
+}
+
+exports.showCmd = (rl,id) => {
+
+	if (typeof id === "undefined") {
+		errorlog(`Falta el parámetro id.`);
+		rl.prompt();
+
+	} else {
+
+
+		try{
+
+			const quiz = model.getByIndex(id);
+			log(` [${colorize(id,'magenta')}]: ${quiz.question} ${colorize('=>','magenta')} ${quiz.answer}`);
+		} catch(error) {
+
+			errorlog(error.message);
+		}
+	}
+
+	rl.prompt();
+
+};
+
+
+exports.addCmd = rl => {
+
+	rl.question(colorize('Introduzca una pregunta: ', 'red'), question => {
+
+		rl.question(colorize('Introduzca la respuesta: ', 'red'), answer => {
+
+			model.add(question,answer);
+			log(` ${colorize('Se ha añadido', 'magenta')}: ${question} ${colorize('=>','magenta')} ${answer}`)
+			rl.prompt();
+		});
+	});
+
+	rl.prompt();
+
+	
+};
+
+exports.deleteCmd = (rl,id) => {
+
+	if (typeof id === "undefined") {
+		errorlog(`Falta el parámetro id.`);
+		rl.prompt();
+
+	} else {
+
+
+		try{
+			model.deleteByIndex(id);
+		} catch(error) {
+
+			errorlog(error.message);
+		}
+	}
+	rl.prompt();
+
+
+
+};
+
+exports.editCmd = (rl,id) => {
+
+	if (typeof id === "undefined") {
+		errorlog(`Falta el parámetro id.`);
+		rl.prompt();
+
+	} else {
+
+
+		try{
+			const quiz = model.deleteByIndex(id);
+			process.stdout.isTTY && setTimeout(() => {rl.write(quiz.question)},0)
+
+			rl.question(colorize('Introduzca una pregunta: ', 'red'), question => {
+
+				process.stdout.isTTY && setTimeout(() => {rl.write(quiz.answer)},0)
+
+				rl.question(colorize('Introduzca la respuesta: ', 'red'), answer => {
+
+					model.update(id,question,answer);
+					log(` Se ha cambiado el quiz ${colorize(id,'magenta')} por: ${question} ${colorize('=>','magenta')} ${answer}`);
+
+					rl.prompt();
+
+				});
+			});		
+		} catch(error) {
+
+			errorlog(error.message);
+			rl.prompt();
+		}
+	}
+};
+// TO DO 
+exports.testCmd = (rl,id) => {
+
+	if (typeof id === "undefined") {
+		errorlog(`Falta el parámetro id.`);
+		rl.prompt();
+
+	} else {
+
+
+		try {
+
+			quiz = model.getByIndex(id);
+			rl.question(colorize(quiz.question + "? ",'red'), answer => {
+
+			log('Su respuesta es: ');
+			if( answer.toLowerCase().trim() === quiz.answer.toLowerCase()) {
+				biglog('Correcta','green');
+			} else {
+				biglog('Incorrecta','red');
+
+			}
+			rl.prompt();
+
+			});
+
+		} catch (error) {
+
+			errorlog(error.message);
+			rl.prompt();
+
+		}
+	}
+	
+
+};
+
+// TO DO
+exports.playCmd = (rl) => {
+
+	let score = 0;
+	let toBeResolved = [];
+
+	for (let i = 0; i < model.count(); ++i) {
+		toBeResolved[i] = i;
+		
+		
+		};
+
+	const playOne = () => {
+	if (toBeResolved.length === 0){
+		log('Ya no quedan preguntas.');
+		log('Su puntuación es: ');
+		biglog(score);
+		rl.prompt();
+
+
+
+	} else {
+		let id = Math.round(Math.random()*(toBeResolved.length-1));
+		let quiz = model.getByIndex(toBeResolved[id]);
+		toBeResolved.splice(id,1);
+
+		rl.question(colorize(quiz.question + "? ",'red'), answer => {
+
+			log('Su respuesta es: ');
+			if( answer.toLowerCase().trim() === quiz.answer.toLowerCase()) {
+				biglog('Correcta','green');
+				score++;
+				playOne();
+
+			} else {
+				biglog('Incorrecta','red');
+				log('Su puntuación es: ');
+				biglog(score);
+				rl.prompt();
+
+
+
+			}
+
+			});
+
+
+
+		}
+	}
+	playOne();
+
+
+
+};
+
+
+exports.creditsCmd = () => {
+	log('Autores de la práctica:');
+	log('kevinnivek96','green');
+};
+	
+exports.quitCmd = (rl) => {
+	rl.close();
+	
+
+};
